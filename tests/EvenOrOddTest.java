97de3f7b038aee9b5ad90c62e607ab1ac4c6ac15
Yassin Mahgoub <mahgob601@gmail.com>
2022-05-04 10:51:19 +0200

    updated

--- tests/EvenOrOddTest.java
+++ tests/EvenOrOddTest.java
@@ -2,48 +2,62 @@ import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-
+import org.junit.jupiter.api.Nested;
 import static org.junit.Assert.*;
 
 public class EvenOrOddTest {
 
     EvenOrOdd tester;
+
     @BeforeEach
     public void before()
     {
         tester = new EvenOrOdd();
     }
 
-    @Test
-    public void evenOddEvenTest1()
+    @Nested
+    class EvenTesting
     {
-        EvenOrOdd tester1 = new EvenOrOdd();
-        Assert.assertEquals("Even Number", tester1.evenOdd(4));
+        @Test
+        public void evenOddEvenTest1()
+        {
+            EvenOrOdd tester1 = new EvenOrOdd();
+            Assert.assertEquals("Even Number", tester1.evenOdd(4));
+
+        }
+
+        @Test
+        public void evenOddEvenTest2()
+        {
+            EvenOrOdd tester2 = new EvenOrOdd();
+            Assert.assertEquals("Even Number", tester2.evenOdd(20));
+
+        }
 
     }
 
-    @Test
-    public void evenOddEvenTest2()
+
+    @Nested
+    class OddTesting
     {
-        EvenOrOdd tester2 = new EvenOrOdd();
-        Assert.assertEquals("Even Number", tester2.evenOdd(20));
+        @Test
+        public void evenOddOddTest1()
+        {
+
+            assertEquals("Odd Number", tester.evenOdd(5));
+
+        }
+
+        @Test
+        public void evenOddOddTest2()
+        {
+            assertEquals("Odd Number", tester.evenOdd(37));
+
+        }
 
     }
 
-    @Test
-    public void evenOddOddTest1()
-    {
 
-        assertEquals("Odd Number", tester.evenOdd(5));
-
-    }
-
-    @Test
-    public void evenOddOddTest2()
-    {
-        assertEquals("Odd Number", tester.evenOdd(37));
-
-    }
 
     @Test
     public void evenOddEvenNonInteger()
